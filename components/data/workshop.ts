52f50969f14fbebd518f4162783e31ad96a4d9c8
Agustina Mena <agusmena60@gmail.com>
2026-07-29 11:26:46 -0300

    feat: agrego cards y carrusel

---
+++ components/data/workshop.ts
@@ -0,0 +1,44 @@
+import { Workshop } from "../types/workshop";
+
+export const workshops: Workshop[] = [
+  {
+    id: 1,
+    title: "Tote bags",
+    edition: "Invierno 2026",
+    image: "/images/workshops/totes.webp",
+    status: "realizado",
+    category: "ediciones",
+  },
+   {
+    id: 2,
+    title: "Yesitos",
+    edition: "Invierno 2026",
+    image: "/images/workshops/yesitos.webp",
+    status: "realizado",
+    category: "ediciones",
+  },
+    {
+    id: 3,
+    title: "Bastidores",
+    edition: "Invierno 2026",
+    image: "/images/workshops/bastidores.webp",
+    status: "realizado",
+    category: "ediciones",
+  },
+  {
+    id: 4,
+    title: "Casitas Navideñas",
+    edition: "Navidad 2025",
+    image: "/images/workshops/casitasNav.webp",
+    status: "realizado",
+    category: "ediciones",
+  },
+   {
+    id: 5,
+    title: "Adornos Navideños",
+    edition: "Navidad 2025",
+    image: "/images/workshops/ador.webp",
+    status: "realizado",
+    category: "ediciones",
+  },
+];
